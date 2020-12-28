853cb2b3f5745380e00bc14323e828c50c5ba345
MatthewTee <teelucksingh.matthew1@gmail.com>
2020-12-27 21:31:15 -0400

    Heroku Config

--- django_project/autodidacticism/autodidacticism/prod_settings.py
+++ django_project/autodidacticism/autodidacticism/prod_settings.py
@@ -145,5 +145,10 @@ MARKDOWNIFY_WHITELIST_TAGS = {
 # WhiteNoise Configuation:
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Configuring postgres database:
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(prod_db)
+
 # Activate Django-Heroku.
 django_heroku.settings(config=locals(), staticfiles=False, logging=False)
