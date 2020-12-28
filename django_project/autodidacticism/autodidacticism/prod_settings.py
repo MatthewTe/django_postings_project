c5b49b8f81f6aeb956c299a52a2ac02f6c6e39d9
MatthewTee <teelucksingh.matthew1@gmail.com>
2020-12-27 21:12:55 -0400

    Heroku Configs

--- django_project/autodidacticism/autodidacticism/prod_settings.py
+++ django_project/autodidacticism/autodidacticism/prod_settings.py
@@ -13,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w)0kz6!a_sur&w5+_2%pgg&6=2-v0pyc+)j%+8i-!*i_85_^1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
 
     # heroku Host:
-    "autodidacticism.herokuapp.com"
+    ".herokuapp.com"
 
 ]
 
